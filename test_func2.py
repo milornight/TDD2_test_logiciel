ae2f59c00f2b285f0daa92c0480de78760af4032
LynneYeung959 <jan.kry959@gmail.com>
2022-01-19 15:33:17 +0100

    add some tests for funcion 2

--- test_func2.py
+++ test_func2.py
@@ -5,9 +5,12 @@ class TestFuncs(unittest.TestCase):
 
     def test_deriver(self):
         self.assertEqual(func2.deriver([2.5,3,4.5]),[6.25,3])
+        self.assertEqual(func2.deriver([2.5,3,4.5]),[2.5*2.5,3])
         self.assertEqual(func2.deriver([3,2.5],[3]))
         self.assertEqual(func2.deriver([2.5]),[0])
-        self.assertFalse(func2.deriver([2],None)) # return est vide
+        self.assertFalse(func2.deriver([2],None)) # return est vide au lieu de la liste [0]
+        self.assertEqual(func2.deriver(["a"],False)) # Entrée n'est pas une liste float
+        self.assertEqual(func2.deriver([],None)) # Entrée est une liste vide
 
 if __name__ == '__main__':
     unittest.main()
