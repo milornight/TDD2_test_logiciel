852c79bf6a7b99333525e120e0b79c9b994c121c
milornight <qianhui.jin@yahoo.fr>
2022-01-19 19:55:23 +0100

    some correction for the test function second derive

--- test_funcs3.py
+++ test_funcs3.py
@@ -13,8 +13,8 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs3.derive2([0.0]),None) # trop peu de points pour determiner la derive seconde 
 
         self.assertEqual(funcs3.derive2([1.0, 2.0, 3.0]),[0.0])
-        self.assertEqual(funcs3.derive2([2.3, 5.7, 10.2]),[1.1])
-        self.assertEqual(funcs3.derive2([1.0, 2.3, 5.7, 10.2, 29.6]),[2.1, 1.1, 14,9])
+        self.assertEqual(funcs3.derive2([2.3, 5.7, 10.0]),[0.9])
+        self.assertEqual(funcs3.derive2([1.0, 2.3, 5.7, 10.2, 29.6]),[2.1, 1.1, 14.9])
 
 if __name__ == '__main__':
     unittest.main()
