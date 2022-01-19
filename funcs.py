92cd222e644c0de253dc7af516ba3ebfc686bb37
milornight <qianhui.jin@yahoo.fr>
2022-01-19 13:30:37 +0100

    complete the function miroir

--- funcs.py
+++ funcs.py
@@ -1,4 +1,15 @@
 import string
+import math
 
 def miroir(s,n):
-    return None
+    l = len(s)
+    miroir = ''
+    if l == 0:
+        return None
+    elif l <= n:
+        return False
+    else:
+        miroir = s[0:n+1]
+        for i in range(n,-1,-1):   
+            miroir = miroir + s[i]
+        return miroir
