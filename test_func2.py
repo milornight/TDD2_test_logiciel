9fa984772c5bf88829e395e4ab78a5a741a7df40
LynneYeung959 <jan.kry959@gmail.com>
2022-01-19 16:12:40 +0100

    modifie some errors

--- test_func2.py
+++ test_func2.py
@@ -4,19 +4,21 @@ import unittest
 class TestFuncs(unittest.TestCase):
 
     def test_deriver(self):
+        # Entrée n'est pas une liste float
+        self.assertEqual(func2.deriver([2]), False) 
+        self.assertEqual(func2.deriver(["a"]), False)
+        self.assertEqual(func2.deriver([1.0, "a", 3.0]), False)
+        self.assertEqual(func2.deriver([1.0, 2, 3.0]), False)
+        # Entrée est une liste vide
+        self.assertEqual(func2.deriver([]),None)
+        # Test pour les résultats
         self.assertEqual(func2.deriver([2.5, 3.0, 4.5]), [6.25, 3.0])
         self.assertEqual(func2.deriver([2.5, 3.0, 4.5]), [2.5*2.5, 3.0])
         self.assertEqual(func2.deriver([3.0, 2.5], [3.0]))
         self.assertEqual(func2.deriver([2.5]), [0])
         self.assertFalse(func2.deriver([2.0], None)) # return est vide au lieu de la liste [0]
         self.assertFalse(func2.deriver([2.0], False)) # return est vide au lieu de la liste [0]
-        # Entrée n'est pas une liste float
-        self.assertEqual(func2.deriver([2], False)) 
-        self.assertEqual(func2.deriver(["a"], False))
-        self.assertEqual(func2.deriver([1.0, "a", 3.0], False))
-        self.assertEqual(func2.deriver([1.0, 2, 3.0], False))
-        # Entrée est une liste vide
-        self.assertEqual(func2.deriver([],None)) 
+
 
 if __name__ == '__main__':
     unittest.main()
