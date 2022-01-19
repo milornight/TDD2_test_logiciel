8bb242b76ab5b977043f3f84249b2a731e9f0a30
milornight <qianhui.jin@yahoo.fr>
2022-01-19 13:11:56 +0100

    some modification for test miroir

--- test_funs.py
+++ test_funs.py
@@ -4,10 +4,10 @@ import unittest
 class TestFuncs(unittest.TestCase):
 
     def test_miroir(self):
-        self.assertEquale(funcs.miroir("shfenk",4),"shfennefhs")
-        self.assertEquale(funcs.miroir("s",0),"ss")
+        self.assertEqual(funcs.miroir("shfenk",4),"shfennefhs")
+        self.assertEqual(funcs.miroir("s",0),"ss")
         self.assertFalse(funcs.miroir("sdhj",5)) # indice trop grand
-        self.self.assertEquale(funcs.miroir("",0),None) # string empty
+        self.assertEqual(funcs.miroir("",0),None) # string empty
 
 if __name__ == '__main__':
     unittest.main()
