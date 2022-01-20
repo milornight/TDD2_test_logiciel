bf819188ab6ce34a70e7175b3e996691f3082d40
LynneYeung959 <jan.kry959@gmail.com>
2022-01-20 12:38:21 +0100

    add some tests for input type

--- test_func2.py
+++ test_func2.py
@@ -12,16 +12,16 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(func2.deriver([1.0, 2]), False) # La taille de l'entrée est correcte, mais contient une élément non compatible
         self.assertEqual(func2.deriver([1.0, 1+5j, 3.0]), False) # Entrée contient un élément non compatible
         self.assertEqual(func2.deriver([1.0, "a", 3.0]), False)
+        self.assertEqual(func2.deriver([1, 2.0, 3.0]), False)
         self.assertEqual(func2.deriver([1.0, 2.0, 3]), False)
-         
+        self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5]), False) # La taille d'entrée est très grande
         # Entrée est une liste vide
         self.assertIsNone(func2.deriver([]))
         # Entrée est vide
         self.assertIsNone(func2.deriver(None))
+
         # Test pour les résultats
-        self.assertEqual(func2.deriver([2.5]), [0])
         self.assertEqual(func2.deriver([3.0, 2.5]), [3.0])
         self.assertEqual(func2.deriver([2.5, 3.0, 4.5]), [2*2.5, 3.0])
-        self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5]), [3*5.0, 2*2.5, 3.0])
-        self.assertEqual(func2.deriver([1.0, 5.0, 2.5, 3.0, 4.5]), [4*1.0, 3*5.0, 2*2.5, 3.0])
-        self.assertEqual(func2.deriver([3.9, 1.0, 5.0, 2.5, 3.0, 4.5]), [5*3.9, 4*1.0, 3*5.0, 2*2.5, 3.0])
+        self.assertEqual(func2.deriver([1.0, 5.0, 2.5]), [4*1.0, 3*5.0, 2*2.5, 3.0])
+        self.assertEqual(func2.deriver([3.9, 1.0, 5.0]), [5*3.9, 4*1.0, 3*5.0, 2*2.5, 3.0])
