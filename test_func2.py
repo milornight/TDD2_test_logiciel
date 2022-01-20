8a942ffda5067bc3b960d554317707a019663266
LynneYeung959 <jan.kry959@gmail.com>
2022-01-20 13:42:57 +0100

    modifie words with accent

--- test_func2.py
+++ test_func2.py
@@ -4,13 +4,13 @@ import unittest
 class TestFuncs(unittest.TestCase):
 
     def test_deriver(self):
-        # Entrée n'est pas une liste float
-        # Entrée ne contient qu'une seul valeur
+        # Entree n'est pas une liste float
+        # Entree ne contient qu'une seul valeur
         self.assertEqual(func2.deriver([2]), None) 
         self.assertEqual(func2.deriver(["a"]), None) 
         self.assertEqual(func2.deriver([1+2j]), None) 
         self.assertEqual(func2.deriver([2.0]), None) 
-        # La taille de l'entrée est correcte, mais contient une élément non compatible
+        # La taille de l'entree est correcte, mais contient une element non compatible
         self.assertEqual(func2.deriver([1, 2.0]), False)
         self.assertEqual(func2.deriver(["a", 1.0]), False)
         self.assertEqual(func2.deriver([1+2j, 1.0]), False)
@@ -22,15 +22,15 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(func2.deriver([1.0, 3.0, 1+5j]), False) 
         self.assertEqual(func2.deriver([1.0, 3.0, "a"]), False)
         self.assertEqual(func2.deriver([1.0, 2.0, 3]), False)
-        # la taille d'entrée est trop grande
+        # la taille d'entree est trop grande
         self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5]), None)
         self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5, 6.0]), None)
-        # Entrée est une liste vide
+        # Entree est une liste vide
         self.assertIsNone(func2.deriver([]))
-        # Entrée est vide
+        # Entree est vide
         self.assertIsNone(func2.deriver(None))
 
-        # Test pour les résultats
+        # Test pour les resultats
         self.assertEqual(func2.deriver([3.0, 2.5]), 0.5)
         self.assertEqual(func2.deriver([2.5, 3.0, 4.5]), -0.11)
         self.assertEqual(func2.deriver([1.0, 5.0, 2.5]), -1.6)
