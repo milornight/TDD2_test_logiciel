d03261f1bb91e182237f31ebe296dd89c32cc8d3
LynneYeung959 <jan.kry959@gmail.com>
2022-01-20 12:29:48 +0100

    modify tests on input parameters

--- test_func2.py
+++ test_func2.py
@@ -8,9 +8,12 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(func2.deriver([2]), False) # Entrée int
         self.assertEqual(func2.deriver(["a"]), False) # Entrée char
         self.assertEqual(func2.deriver([1+2j]), False) # Entrée complex
-        self.assertEqual(func2.deriver([1.0, 1+5j, 3.0]), False)
+        self.assertEqual(func2.deriver([2.0]), False) # Entrée ne contient qu'une seul valeur
+        self.assertEqual(func2.deriver([1.0, 2]), False) # La taille de l'entrée est correcte, mais contient une élément non compatible
+        self.assertEqual(func2.deriver([1.0, 1+5j, 3.0]), False) # Entrée contient un élément non compatible
         self.assertEqual(func2.deriver([1.0, "a", 3.0]), False)
-        self.assertEqual(func2.deriver([1.0, 2, 3.0]), False)
+        self.assertEqual(func2.deriver([1.0, 2.0, 3]), False)
+         
         # Entrée est une liste vide
         self.assertIsNone(func2.deriver([]))
         # Entrée est vide
