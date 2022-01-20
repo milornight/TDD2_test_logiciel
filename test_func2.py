4854c59f7075310565840abc0d0464963fee041f
LynneYeung959 <jan.kry959@gmail.com>
2022-01-20 12:52:12 +0100

    add tests for input

--- test_func2.py
+++ test_func2.py
@@ -5,16 +5,26 @@ class TestFuncs(unittest.TestCase):
 
     def test_deriver(self):
         # Entrée n'est pas une liste float
-        self.assertEqual(func2.deriver([2]), False) # Entrée int
-        self.assertEqual(func2.deriver(["a"]), False) # Entrée char
-        self.assertEqual(func2.deriver([1+2j]), False) # Entrée complex
-        self.assertEqual(func2.deriver([2.0]), False) # Entrée ne contient qu'une seul valeur
-        self.assertEqual(func2.deriver([1.0, 2]), False) # La taille de l'entrée est correcte, mais contient une élément non compatible
-        self.assertEqual(func2.deriver([1.0, 1+5j, 3.0]), False) # Entrée contient un élément non compatible
-        self.assertEqual(func2.deriver([1.0, "a", 3.0]), False)
+        # Entrée ne contient qu'une seul valeur
+        self.assertEqual(func2.deriver([2]), None) 
+        self.assertEqual(func2.deriver(["a"]), None) 
+        self.assertEqual(func2.deriver([1+2j]), None) 
+        self.assertEqual(func2.deriver([2.0]), None) 
+        # La taille de l'entrée est correcte, mais contient une élément non compatible
+        self.assertEqual(func2.deriver([1, 2.0]), False)
+        self.assertEqual(func2.deriver(["a", 1.0]), False)
+        self.assertEqual(func2.deriver([1+2j, 1.0]), False)
+        self.assertEqual(func2.deriver([1.0, 2]), False) 
+        self.assertEqual(func2.deriver([1.0, "a"]), False) 
+        self.assertEqual(func2.deriver([1.0, 1+2j]), False) 
         self.assertEqual(func2.deriver([1, 2.0, 3.0]), False)
+        self.assertEqual(func2.deriver([1.0, "a", 3]), False)
+        self.assertEqual(func2.deriver([1.0, 3.0, 1+5j]), False) 
+        self.assertEqual(func2.deriver([1.0, 3.0, "a"]), False)
         self.assertEqual(func2.deriver([1.0, 2.0, 3]), False)
-        self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5]), False) # La taille d'entrée est très grande
+        # la taille d'entrée est trop grande
+        self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5]), None)
+        self.assertEqual(func2.deriver([5.0, 2.5, 3.0, 4.5, 6.0]), None)
         # Entrée est une liste vide
         self.assertIsNone(func2.deriver([]))
         # Entrée est vide
