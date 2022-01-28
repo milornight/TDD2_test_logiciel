01e5063f8830a18735bde5fa85b422eebc49c1a8
LynneYeung959 <73192171+LynneYeung959@users.noreply.github.com>
2022-01-28 10:56:21 +0100

    :art: Modify error return content

--- test_func4.py
+++ test_func4.py
@@ -4,36 +4,37 @@ import unittest
 
 class TestFuncs(unittest.TestCase):
 
-    def test_approximatipn(self):
+    def test_entrees(self):
         # Test les parametres
         # Test pour les parametres insuffisant
-        self.assertEqual(func4.appro(None, 3.0, 0.1), "Error: Missing parameters")
-        self.assertEqual(func4.appro(math.sin, None, None), "Error: Missing parameters") 
-        self.assertEqual(func4.appro(math.sin, 3.0, None), "Error: Missing parameters")
+        self.assertEqual(func4.appro(None, 3.0, 0.1), 200)
+        self.assertEqual(func4.appro(math.sin, None, None), 200) 
+        self.assertEqual(func4.appro(math.sin, 3.0, None), 200)
 
         # Test pour le premier parametre non valid
-        # 1er parametre n'est pas une liste
-        self.assertEqual(func4.appro(4, 3.0, 0.01), "Error: The first parameter is not a function")
-        self.assertEqual(func4.appro(3.0, 3.0, 0.01), "Error: The first parameter is not a function")
-        self.assertEqual(func4.appro("a", 3.0, 0.01), "Error: The first parameter is not a function")
-        self.assertEqual(func4.appro(1+2j, 3.0, 0.01), "Error: The first parameter is not a function")
+        # 1er parametre n'est pas une fonction
+        self.assertEqual(func4.appro(4, 3.0, 0.01), 210)
+        self.assertEqual(func4.appro(3.0, 3.0, 0.01), 210)
+        self.assertEqual(func4.appro("a", 3.0, 0.01), 210)
+        self.assertEqual(func4.appro(1+2j, 3.0, 0.01), 210)
 
         # Test pour le 2e parametre
         # 2e parametre n'est pas un float
-        self.assertEqual(func4.appro(math.sin, 3, 0.01), "Error: The second parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, "a", 0.01), "Error: The second parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, 1+2j, 0.01), "Error: The second parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, [3.0], 0.01), "Error: The second parameter is not a float")
+        self.assertEqual(func4.appro(math.sin, 3, 0.01), 220)
+        self.assertEqual(func4.appro(math.sin, "a", 0.01), 220)
+        self.assertEqual(func4.appro(math.sin, 1+2j, 0.01), 220)
+        self.assertEqual(func4.appro(math.sin, [3.0], 0.01), 220)
 
         # Test pour le 3e parametre
         # ce n'est pas un ordre de grandeur
-        self.assertEqual(func4.appro(math.sin, 3.0, 1), "Error: The third parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, 3.0, "a"), "Error: The third parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, 3.0, 1+2j), "Error: The third parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, 3.0, [3]), "Error: The third parameter is not a float")
-        self.assertEqual(func4.appro(math.sin, 3.0, 0.02), "Error: The third parameter is not a order of magnitude")
-        self.assertEqual(func4.appro(math.sin, 3.0, 1.0), "Error: The third parameter is not a order of magnitude")
+        self.assertEqual(func4.appro(math.sin, 3.0, 1), 230)
+        self.assertEqual(func4.appro(math.sin, 3.0, "a"), 230)
+        self.assertEqual(func4.appro(math.sin, 3.0, 1+2j), 230)
+        self.assertEqual(func4.appro(math.sin, 3.0, [3]), 230)
+        self.assertEqual(func4.appro(math.sin, 3.0, 0.02), 231)
+        self.assertEqual(func4.appro(math.sin, 3.0, 1.0), 231)
 
+    def test_fonc_arrondi(self):
         # Test pour la fonction d'arrondi
         self.assertEqual(func4.arrondi(1.456, 0.1), 1.5)
         self.assertEqual(func4.arrondi(1.456, 0.01), 1.46)
@@ -43,6 +44,7 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(func4.arrondi(1.499, 0.1), 1.5)
         self.assertEqual(func4.arrondi(1.499, 0.01), 1.50)
 
+    def test_approximation(self):
         # Test pour le resultat
         self.assertEqual(func4.appro(math.sin, 3.0, 0.1), -1.0)
         self.assertEqual(func4.appro(math.sin, 3.0, 0.01), -0.99) # avec un ordre de grandeur plus petit
